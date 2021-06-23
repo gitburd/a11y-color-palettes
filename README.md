1c9d87dd44c729f9c57b148f4793671eb7b37976
Dena Burd <denaburd@gmail.com>
2021-06-23 11:20:12 -0700

    Rearranges the about page. Updates the README

--- README.md
+++ README.md
@@ -32,5 +32,3 @@ Runs the app in the development mode.<br />
 Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 
 
-### Ally Palette was made as part of a <a href="https://mlh.io/" target="_blank" rel="noopener">Major League Hacking</a> Hackathon!
-
--- src/components/About.js
+++ src/components/About.js
@@ -7,65 +7,67 @@ import { Link } from 'react-router-dom'
 const About = () => {
     return (
         <article style={{
-            margin: '30px auto',
-            width: '60%',
+            margin: '30px auto'
         }}>
-            <h1
-                style={{ textAlign: 'center', color: '#9506a0' }}
-            >
-                A11y Palette
-            </h1>
-            <h2
-                style={{ textAlign: 'center' }}
-            >
-                Create contrasting color palettes
-            </h2>
-            <p style={{ paddingTop: '20px' }}>
-                - Get suggested contrasting colors for any color
-            </p>
-            <p>
-                - Test background and text color pairs for {' '}
-                <a
-                    href="https://www.w3.org/WAI/standards-guidelines/wcag/"
-                    target="_blank"
-                    rel="noreferrer"
-                    className='link'
-                >
-                    WCAG AA and AAA
-                </a> a11y guidline compliance
-            </p>
-            <p>
-                - Save your palettes
-            </p>
-            <br />
+            <section style={{ textAlign: 'center', marginTop: '20px auto' }}>
+                <h1>
+                    Create contrasting color palettes
+                </h1>
 
-            <Link to='/examples'><h2>Example Palettes</h2></Link>
+                <Link to='/examples'><h2>Example Palettes</h2></Link>
+            </section>
 
-            <h2 id="demos">Demos</h2>
-            <p>Making a palette from a color</p>
-            <video width="90%" controls >
-                <source src={paletteFromColor} type="video/mp4" />
-            </video>
 
-            <p>Making a palette from an example palette</p>
-            <video width="90%" controls >
-                <source src={paletteFromPalette} type="video/mp4" />
-            </video>
+            <section className="about-box">
+                <p >
+                    - Get suggested contrasting colors for any color
+                </p>
+                <p>
+                    - Test background and text color pairs for {' '}
+                    <a
+                        href="https://www.w3.org/WAI/standards-guidelines/wcag/"
+                        target="_blank"
+                        rel="noreferrer"
+                        className='link'
+                    >
+                        WCAG AA and AAA
+                    </a> a11y guidline compliance
+                </p>
+                <p>
+                    - Save your palettes
+                </p>
+            </section>
 
-            <h2>Why is this important?</h2>
-            <p>
-                <a
-                    href="https://www.w3.org/WAI/standards-guidelines/wcag/"
-                    target="_blank"
-                    rel="noreferrer"
-                    className='link'
-                >
-                    WCAG AA and AAA
-                </a>
-                {' '}
-                accessibility guidlines help designers pick text and background colors that have enough luminosity contrast to be easily read by people how experince low vision or don't see color.
-                There are differnt standards for large and bold text than for small text.
-            </p>
+            <section className="demos">
+                <h2>Demos</h2>
+                <p>Making a palette from a color</p>
+                <video width="100%" controls >
+                    <source src={paletteFromColor} type="video/mp4" />
+                </video>
+
+                <p>Making a palette from an example palette</p>
+                <video width="100%" controls >
+                    <source src={paletteFromPalette} type="video/mp4" />
+                </video>
+
+            </section>
+            <section className="demos">
+                <h2>Why is this important?</h2>
+                <p>
+                    <a
+                        href="https://www.w3.org/WAI/standards-guidelines/wcag/"
+                        target="_blank"
+                        rel="noreferrer"
+                        className='link'
+                    >
+                        WCAG AA and AAA
+                    </a>
+                    {' '}
+                    accessibility guidlines help designers pick text and background colors that have enough luminosity contrast to be easily read by people how experince low vision or don't see color.
+                    There are differnt standards for large and bold text than for small text.
+                </p>
+
+            </section>
         </article>
     );
 };
